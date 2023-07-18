c8835edf48bc35273e1b5426e81ee57f3b5455d2
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2023-07-18 01:36:22 +0100

    Extracted common CSS

--- src/templates/html.mjs
+++ src/templates/html.mjs
@@ -12,6 +12,7 @@ export function htmlTpl(head, body, meta = {}) {
       <meta encoding="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/main.css">
       ${Object.entries(mergeMeta(meta)).map(([name, content]) => /*html*/`
         <meta name="${name}" content="${content}">
       `)}
